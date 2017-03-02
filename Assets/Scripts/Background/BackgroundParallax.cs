e77fc7e338a967c3544db8b4fc04c92152b5b2b0
rmorocz <ricardomorocz@gmail.com>
2017-03-01 19:27:49 -0500

    Adding Background Parallaxing
    A new script was written to enable parallax scrolling for the multiple
    backgrounds. This creates a sense of visual depthness.

---
+++ Assets/Scripts/Background/BackgroundParallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+
+	public Vector2 scrollSpeed;
+	private Vector2 offset;
+	private MeshRenderer myMeshRenderer;
+
+	void Start () 
+	{
+		myMeshRenderer = gameObject.GetComponent<MeshRenderer> ();
+		offset = myMeshRenderer.material.GetTextureOffset ("_MainTex");
+	}
+
+	void Update () 
+	{
+		offset += scrollSpeed * Time.deltaTime;
+		myMeshRenderer.material.SetTextureOffset("_MainTex", offset);
+	}
+}
