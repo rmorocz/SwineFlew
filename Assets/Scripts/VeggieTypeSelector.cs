c36406951f3b0671bae1b98f741bf3ee7a38a87e
rmorocz <ricardomorocz@gmail.com>
2017-03-08 21:17:12 -0500

    Rate based Randomization for Veggie selection

--- Assets/Scripts/VeggieTypeSelector.cs
+++ Assets/Scripts/VeggieTypeSelector.cs
@@ -7,20 +7,44 @@ public class VeggieTypeSelector : MonoBehaviour {
 	//SPRITE VARIABLES
 	private SpriteRenderer mySpriteRenderer;
 	public Sprite[] sprites;
-	private int spriteTypes;
+
 	public int selectedSprite;
+
+
+	[Header("Rate of Appearance")]
+	public int carrotRate;
+	public int beetRate;
+	public int pepperRate;
+
+
+	private int spriteTypes;
+
+	private int randomSelection;
 
 	void Awake () 
 	{
-		mySpriteRenderer = GetComponent <SpriteRenderer> ();
+		mySpriteRenderer = gameObject.GetComponent <SpriteRenderer> ();
 		spriteTypes = sprites.Length;
-		selectedSprite = Random.Range (0, spriteTypes);
-		mySpriteRenderer.sprite = sprites [selectedSprite];
 	}
 
-	void OnDisable() 
+	void OnEnable() 
 	{
-		selectedSprite = Random.Range (0, spriteTypes);
+		randomSelection = Random.Range (0, 100);				//This value is used to select the veggie type, allowing for different veggie probabilities assignment
+
+		//VEGGIE SELECTION
+		if (randomSelection <= carrotRate) 
+		{
+			selectedSprite = 0;
+		} 
+		else if (randomSelection > carrotRate && randomSelection <= (beetRate + carrotRate)) 
+		{
+			selectedSprite = 1;
+		} 
+		else if (randomSelection > (beetRate + carrotRate))
+		{
+			selectedSprite = 2;
+		}
+
 		mySpriteRenderer.sprite = sprites [selectedSprite];
 	}
 
